0ab451ad50214f072638fff866e26d4b143883ca
Mohamad Abdul Karim Abdouni <dudiabdouni@gmail.com>
2022-04-28 14:39:38 +0300

    Initial Commit

---
+++ TipsyTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipsyTwo
+//
+//  Created by Dudi Abdouni on 28/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
